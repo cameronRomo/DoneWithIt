4703ce8a09461fcdfe55db636c1416def1138d96
Cameron Romo <cromo1225@gmail.com>
2021-05-19 17:09:26 -0600

    Add AppText component to render method

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View, } from 'react-native';
+import { Text, View, } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+import AppText from './app/components/AppText/AppText';
 
 export default function App() {
   return (
-    <ViewImageScreen/>
+    <View 
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <AppText>I love react native</AppText>
+    </View>
     );
 }
