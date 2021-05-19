0253651088b2467a7d7c2a69c0ac6e9003cc7b05
Cameron Romo <cromo1225@gmail.com>
2021-05-19 16:11:55 -0600

    Add homescreen to the render

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { View, } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  let x = 1;
-  console.log("App executed");
-
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+    <ViewImageScreen/>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'dodgerblue',
-  },
-});
